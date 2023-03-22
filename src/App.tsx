76c57d3150cedb95a670d8e8f5f41d88cd3e240e
Humai88 <Humai.Vierdiieva@IRIS.CO.UK>
2023-03-22 13:11:46 +0000

    add sub-text section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './App.module.scss';
 import { HomeSection } from './components/home/HomeSection';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 function App() {
   return (
+    <ParallaxProvider>
     <div className={styles.content}>
       <HomeSection />
     </div>
+    </ParallaxProvider>
   );
 }
 
--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -7,19 +7,41 @@ import { Ways } from '../../UI-kit/Ways';
 import { Technology } from '../../UI-kit/Technology';
 import { Culture } from '../../UI-kit/Culture';
 import ReactTextTransition from 'react-text-transition';
+import { Fade } from 'react-awesome-reveal';
+import { Parallax } from 'react-scroll-parallax';
 
 export const Main = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const keyframes = [
-    { text: '', color: '#fff' },
-    { text: 'talent', color: '#D0E6A4' },
-    { text: 'ways of working', color: '#FB887B' },
-    { text: 'technology', color: '#FEDD95' },
-    { text: 'culture', color: '#CDABDA' },
+    { text: '', color: '#fff', subHeader: '', subText: '' },
+    {
+      text: 'talent',
+      color: '#D0E6A4',
+      subHeader: 'Finding the talent',
+      subText: 'We Leverage the power of highly diverse teams with the skills, backgrounds, expertise to solve complex problems.',
+    },
+    {
+      text: 'ways of working',
+      color: '#FB887B',
+      subHeader: 'Finding ways of working',
+      subText: 'We Leverage the power of highly diverse teams with the skills, backgrounds, expertise to solve complex problems.',
+    },
+    {
+      text: 'technology',
+      color: '#FEDD95',
+      subHeader: 'Finding technology',
+      subText: 'We Leverage the power of highly diverse teams with the skills, backgrounds, expertise to solve complex problems.',
+    },
+    {
+      text: 'culture',
+      color: '#CDABDA',
+      subHeader: 'Finding culture',
+      subText: 'We Leverage the power of highly diverse teams with the skills, backgrounds, expertise to solve complex problems.',
+    },
   ];
 
-  const intervalTime = 3000;
+  const intervalTime = 2000;
   useEffect(() => {
     if (currentIndex === keyframes.length) {
       setCurrentIndex(0);
@@ -34,40 +56,60 @@ export const Main = () => {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.textWrapper}>
-        {currentIndex !== 0 ? (
-          <>
-            <h2 className={styles.text}>combining</h2>
-            <div>
-              <ReactTextTransition
-                className={styles.coloredText}
-                style={{
-                  color: `${keyframes[currentIndex % keyframes.length].color}`,
-                }}
-              >
-                {keyframes[currentIndex % keyframes.length].text}
-              </ReactTextTransition>
-            </div>
-            <h2 className={styles.text}>for innovation</h2>
-          </>
-        ) : (
-          <h2
-            className={`${styles.text} ${styles.mainHeader}`}
-            style={{ color: `${keyframes[0].color}` }}
-          >
-            We Leverage <br /> the power of highly
-            <br /> diverse teams
-          </h2>
-        )}
-      </div>
-      <img src={main} />
-      {/* <div className={styles.imageWrapper}>
+      <Parallax speed={5}>
+        <div className={styles.textWrapper}>
+          {currentIndex !== 0 ? (
+            <>
+              <h2 className={styles.text}>combining</h2>
+              <div>
+                <ReactTextTransition
+                  className={styles.coloredText}
+                  style={{
+                    color: `${
+                      keyframes[currentIndex % keyframes.length].color
+                    }`,
+                  }}
+                >
+                  {keyframes[currentIndex % keyframes.length].text}
+                </ReactTextTransition>
+              </div>
+              <h2 className={styles.text}>for innovation</h2>
+              <div className={styles.subText}>
+                <h3>{keyframes[currentIndex % keyframes.length].subHeader}</h3>
+                <div style={{backgroundColor: `${keyframes[currentIndex % keyframes.length].color}`}}></div>
+                <span>{keyframes[currentIndex % keyframes.length].subText}</span>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className={styles.mainHeader}>
+                <h2
+                  className={styles.text}
+                  style={{ color: `${keyframes[0].color}` }}
+                >
+                  We Leverage <br /> the power of highly
+                  <br /> diverse teams
+                </h2>
+                <Fade direction="up" duration={1400}>
+                  <span className={styles.subHeader}>
+                    skills to solve complex problems for our forward thinking
+                    partners
+                  </span>
+                </Fade>
+              </div>
+            </>
+          )}
+        </div>
+
+        <img src={main} />
+        {/* <div className={styles.imageWrapper}>
         <BackgroundMain />
         <Talents />
         <Ways />
         <Technology />
         <Culture />
       </div> */}
+      </Parallax>
     </div>
   );
 };
